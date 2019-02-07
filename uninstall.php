71ad68eda5cc5a31569692380eab3e14fa92b4d2
guisopo <solerguillermo@hotmail.com>
2019-02-07 20:26:42 +0100

    Created uninstall file that manages via SQL the process

---
+++ uninstall.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Trigger this file on Plugin uninstall
+ * 
+ * @package GuisopoPlugin
+ * 
+ */
+
+ // First we made ALWAYS this security check
+ // If WP_UNINSTALL_PLUGIN is not defined somebody is trying to acces uninstall.php not via WordPress
+ // 
+ if( ! definded( 'WP_UNINSTALL_PLUGIN' ) ) {
+  die;
+ }
+
+ // Clear DB stored data
+
+//  $books = get_post( array( 'post_type' => 'books', 'numberofposts' => -1 ) );
+//  foreach($books as $ book) {
+//   wp_delete_post($book->ID, true);
+//  }
+
+// Access DB via SQL
+global $wpdb;
+
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" );
+$wpdb->query( "DELETE FROM wp_postsmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
+$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );

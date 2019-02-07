c20b8d6daab9dc53907258c71b22365ddbe6b6d4
guisopo <solerguillermo@hotmail.com>
2019-02-07 20:13:25 +0100

    Added plugin information

---
+++ guisopo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package AwesomePlugin
+ */
+
+ /*
+  Plugin Name: Awesome Plugin
+  Plugin URI: http://www.guisopo.com/awesome
+  Description: Tutorial plugin
+  Version: 1.0.0
+  Author: Guisopo
+  Author URI: http://www.guisopo.com
+  License: GPLv2 or later
+  Text Domain: http://www.guisopo.com/awesome
+  */

aeb34cbaeaac0df49d16d6717e37cbdac17da3c9
guisopo <solerguillermo@hotmail.com>
2019-02-07 20:14:32 +0100

    Added security check

--- guisopo.php
+++ guisopo.php
@@ -13,3 +13,8 @@
   License: GPLv2 or later
   Text Domain: http://www.guisopo.com/awesome
   */
+
+// ABSPATH  is a constant variable defined by WP when initializing WP site and carries
+  // itself during the WP installation ONLY IF is the software itself who is accesing the php file.
+  // If something else external from the website is accesing those files ABSPATH is not defined
+  defined ( 'ABSPATH' ) or die('You cannot acces this file!');

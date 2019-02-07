7fc8577a8deb015e1f4e24f49778296949b54edd
guisopo <solerguillermo@hotmail.com>
2019-02-07 20:18:12 +0100

    Created new class which creates new post type

--- guisopo.php
+++ guisopo.php
@@ -1,20 +1,39 @@
 <?php
 /**
- * @package AwesomePlugin
+ * @package GuisopoPlugin
  */
 
  /*
-  Plugin Name: Awesome Plugin
-  Plugin URI: http://www.guisopo.com/awesome
+  Plugin Name: Guisopo Plugin
+  Plugin URI: http://www.guisopo.com/Guisopo
   Description: Tutorial plugin
   Version: 1.0.0
   Author: Guisopo
   Author URI: http://www.guisopo.com
   License: GPLv2 or later
-  Text Domain: http://www.guisopo.com/awesome
+  Text Domain: http://www.guisopo.com/Guisopo
   */
 
 // ABSPATH  is a constant variable defined by WP when initializing WP site and carries
   // itself during the WP installation ONLY IF is the software itself who is accesing the php file.
   // If something else external from the website is accesing those files ABSPATH is not defined
   defined ( 'ABSPATH' ) or die('You cannot acces this file!');
+
+// We check if the class exists. Safety precation recommended in PHP
+if( class_exists( 'GuisopoPlugin' ) ){
+
+  class GuisopoPlugin
+  {
+    protected function create_post_type() {
+      add_action( 'init', array( $this, 'custom_post_type') );
+    }
+
+    function custom_post_type() {
+      register_post_type('book', ['public'=> true, 'label'=> 'Books']);
+    }
+
+  }
+
+  // We store the initialization of the class in a variable because then we can call it in other sections
+  $GuisopoPlugin = new GuisopoPlugin();
+}

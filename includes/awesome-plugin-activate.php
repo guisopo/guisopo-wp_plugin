ceee7744d968216e825b3ace60d7da6b73384e40
guisopo <solerguillermo@hotmail.com>
2019-02-07 20:19:57 +0100

    Created new file with a class to handle the activation of the plugin

---
+++ includes/awesome-plugin-activate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package GuisopoPlugin
+ */
+
+class GuisopoPluginActivate
+{
+  public static function activate() {
+    // create CPT. Find this method only inside the class
+    // $this->custom_post_type();
+    // flush rewrite rules: tells WP something is happenning in the DB and needs to refresh in order to read the new information
+    flush_rewrite_rules();
+  }
+}

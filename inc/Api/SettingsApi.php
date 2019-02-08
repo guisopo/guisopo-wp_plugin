5c8cdd2c5b2f67990b5622c9537a47e7a33b405e
guisopo <solerguillermo@hotmail.com>
2019-02-08 17:22:06 +0100

    Created subpages functionality adding three new subpages

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -9,15 +9,45 @@
 class SettingsApi
 {
   public $admin_pages = array();
+  public $admin_subpages = array();
 
   public function register() {
-    if( ! empty($this->admin_pages ) ) {
+    if( ! empty( $this->admin_pages ) ) {
       add_action( 'admin_menu', array($this, 'addAdminMenu') );
     }
   }
 
   public function addPages( array $pages ) {
     $this->admin_pages = $pages;
+    return $this;
+  }
+
+  public function withSubPage( string $title = null ) {
+
+    if( empty( $this->admin_pages ) ) {
+      return $this;
+    }
+
+    $admin_page = $this->admin_pages[0];
+
+    $subpage = [
+      [
+        'parent_slug' => $admin_page['menu_slug'],
+        'page_title' => $admin_page['page_title'],
+        'menu_title' => ($title) ? $title : $admin_page['menu_title'],
+        'capability' => $admin_page['capability'],
+        'menu_slug' => $admin_page['menu_slug'],
+        'callback' => $admin_page['callback']
+      ]
+    ];
+
+    $this->admin_subpages = $subpage;
+
+    return $this;
+  }
+
+  public function addSubPages( array $pages ) {
+    $this-> admin_subpages = array_merge( $this->admin_subpages, $pages);
     return $this;
   }
 
@@ -30,6 +60,15 @@ class SettingsApi
                         $page['callback'], 
                         $page['icon_url'], 
                         $page['position']
+                    );
+    }
+    foreach( $this->admin_subpages as $subpage) {
+        add_submenu_page( $subpage['parent_slug'],
+                          $subpage['page_title'], 
+                          $subpage['menu_title'], 
+                          $subpage['capability'], 
+                          $subpage['menu_slug'], 
+                          $subpage['callback']
                     );
     }
   }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -11,6 +11,7 @@ class Admin extends BaseController
 {
   public $settings;
   public $pages = array();
+  public $subpages = array();
 
   public function __construct() {
     $this->settings = new SettingsApi();
@@ -20,14 +21,40 @@ class Admin extends BaseController
         'menu_title' => 'Guisopo',
         'capability' => 'manage_options',
         'menu_slug' => 'guisopo_plugin',
-        'callback' => function() {echo '<h1>Hola</h1>'; },
+        'callback' => function() {echo '<h1>Guisopo Plugin</h1>'; },
         'icon_url' => 'dashicons-store',
         'position' => 110
       ]
     ];
+    $this->subpages = [
+      [ 
+        'parent_slug' => 'guisopo_plugin',
+        'page_title' => 'Custom Post Types',
+        'menu_title' => 'CPT',
+        'capability' => 'manage_options',
+        'menu_slug' => 'guisopo_cpt',
+        'callback' => function() {echo '<h1>CPT Manager</h1>'; },
+      ],
+      [ 
+        'parent_slug' => 'guisopo_plugin',
+        'page_title' => 'Custom Taxonomies',
+        'menu_title' => 'Taxonomies',
+        'capability' => 'manage_options',
+        'menu_slug' => 'guisopo_taxonomies',
+        'callback' => function() {echo '<h1>Taxonomies Manager</h1>'; },
+      ],
+      [ 
+        'parent_slug' => 'guisopo_plugin',
+        'page_title' => 'Custom Widgets',
+        'menu_title' => 'Widgets',
+        'capability' => 'manage_options',
+        'menu_slug' => 'guisopo_widgets',
+        'callback' => function() {echo '<h1>Widgets Manager</h1>'; },
+      ]
+      ];
   }
 
   public function register() {
-    $this->settings->addPages( $this->pages )->register();
+    $this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubpages( $this->subpages )->register();
   }
 }

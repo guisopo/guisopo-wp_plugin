2218387fec2cc3df54f9a625d261ae1556b6ab13
guisopo <solerguillermo@hotmail.com>
2019-02-11 16:40:38 +0100

    Added event listener to change tabs

--- templates/admin.php
+++ templates/admin.php
@@ -3,14 +3,14 @@
   <?php settings_errors() ?>
 
   <ul class="nav nav-tabs">
-    <li class="active"><a href="#tab1">Manage Settings</a></li>
-    <li><a href="#tab2">Updates</a></li>
-    <li><a href="#tab3">About</a></li>
+    <li class="active"><a href="#tab-1">Manage Settings</a></li>
+    <li><a href="#tab-2">Updates</a></li>
+    <li><a href="#tab-3">About</a></li>
   </ul>
 
   <div class="tab-content">
 
-    <div id="1" class="tab-pane active">
+    <div id="tab-1" class="tab-pane active">
       <form action="options.php" action="post">
         <?php
           settings_fields( 'guisopo_options_group' ); // setSettings->option_group
@@ -20,11 +20,11 @@
       </form>
     </div>
 
-    <div id="2" class="tab-pane">
+    <div id="tab-2" class="tab-pane">
       <h3>Updates</h3>
     </div>
 
-    <div id="3" class="tab-pane">
+    <div id="tab-3" class="tab-pane">
       <h3>About</h3>
     </div>
 

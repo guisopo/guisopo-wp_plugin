0cf3d9b61ece8678bfafc3fe637b1a4461bb1a66
guisopo <solerguillermo@hotmail.com>
2019-02-12 03:41:04 +0100

    Optimized code

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -9,12 +9,28 @@ namespace Inc\Base;
 class BaseController
 { 
   public $plugin_path;
+
   public $plugin_url;
+
   public $plugin;
+
+  public $managers = array();
 
   public function __construct() {
     $this->plugin_path = plugin_dir_path( dirname( __FILE__, 2) );
     $this->plugin_url = plugin_dir_url( dirname( __FILE__, 2) );
     $this->plugin = plugin_basename( dirname( __FILE__, 3) ) . '/guisopo.php';
+
+    $this->managers = [
+      'cpt_manager' => 'Activate CPT Manager',
+      'taxonomy_manager' => 'Activate Taxonomies Manager',
+      'media_widget' => 'Activate Media Widget',
+      'gallery_manager' => 'Activate Gallery Manager',
+      'testimonial_manager' => 'Activate Testimonial Manager',
+      'templates_manager' => 'Activate Templates Manager',
+      'login_manager' => 'Activate Login Manager',
+      'membership_manager' => 'Activate Membership Manager',
+      'chat_manager' => 'Activate Chat Manager'
+    ];
   }
 }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -80,56 +80,18 @@ class Admin extends BaseController
   }
 
   public function setSettings() {
-    $args = [
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'cpt_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'taxonomy_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'media_widget',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'gallery_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'testimonial_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'tempaltes_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'login_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'memebrship_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        [
-          'option_group' => 'guisopo_plugin_settings',
-          'option_name' => 'chat_manager',  // same exact name of ID of custom field
-          'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
-        ],
-        
-      ];
 
-      $this->settings->setSettings( $args );
+    $args = array();
+
+    foreach($this->managers as $key => $value) {
+      $args[] = array(
+                  'option_group' => 'guisopo_plugin_settings',
+                  'option_name' => $key,  // same exact name of ID of custom field
+                  'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
+                );
+    }
+
+    $this->settings->setSettings( $args );
   }
 
   public function setSections() {
@@ -146,107 +108,19 @@ class Admin extends BaseController
   }
 
   public function setFields() {
-    $args = [
-        [
-          'id' => 'cpt_manager',
-          'title' => 'Activate CPT Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'cpt_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-          ],
-        [
-          'id' => 'taxonomy_manager',
-          'title' => 'Activate Taxonomies Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'taxonomy_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-        [
-          'id' => 'media_widget',
-          'title' => 'Activate Media Widget',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'media_widget', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-        [
-          'id' => 'gallery_manager',
-          'title' => 'Activate Gallery Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'gallery_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-        [
-          'id' => 'testimonial_manager',
-          'title' => 'Acitvate Testimonial Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'testimonial_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-        [
-          'id' => 'templates_manager',
-          'title' => 'Acitivate Templates Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'templates_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-        [
-          'id' => 'login_manager',
-          'title' => 'Activate Login Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'login_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-        [
-          'id' => 'membership_manager',
-          'title' => 'Activate Membership Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'membership_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-        [
-          'id' => 'chat_manager',
-          'title' => 'Activate Chat Manager',
-          'callback'  => array($this->callbacks_manager, 'checkboxField'),
-          'page'  => 'guisopo_plugin',
-          'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
-          'args' => array(
-            'label_for' =>  'chat_manager', // Label should always get the ID in order to get that option for the callback
-            'class' => 'ui-toggle'
-          )
-        ],
-      ];
+    foreach($this->managers as $key => $value) {
+      $args[] = array(
+                  'id' => $key,
+                  'title' => $value,
+                  'callback'  => array($this->callbacks_manager, 'checkboxField'),
+                  'page'  => 'guisopo_plugin',
+                  'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
+                  'args' => array(
+                    'label_for' =>  $key, // Label should always get the ID in order to get that option for the callback
+                    'class' => 'ui-toggle'
+                  )
+                );
+    }
 
       $this->settings->setFields( $args );
   }

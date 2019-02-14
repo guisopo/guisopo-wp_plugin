7da799930789ab620fb5f68cbe04aa2e81e433ad
guisopo <solerguillermo@hotmail.com>
2019-02-14 09:28:27 +0100

    Styled cpt template

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -10,5 +10,18 @@ class Activate
     // $this->custom_post_type();
     // flush rewrite rules: tells WP something is happenning in the DB and needs to refresh in order to read the new information
     flush_rewrite_rules();
-  }
+
+    $default = array();
+
+    if( ! get_option( 'guisopo_plugin' ) ) {
+      update_option( 'guisopo_plugin', $default);
+    }
+
+    if( ! get_option( 'guisopo_plugin_cpt' ) ) {
+      update_option( 'guisopo_plugin_cpt', $default);
+    }
+
+    
+
+  } 
 }
--- templates/cpt.php
+++ templates/cpt.php
@@ -3,13 +3,49 @@
   <h1>Custom Post Types Manager</h1>
   
   <?php settings_errors() ?>
-  
-  <form action="options.php" method="post">
-    <?php
-      settings_fields( 'guisopo_plugin_cpt_settings' );
-      do_settings_sections( 'guisopo_cpt' );
-      submit_button();
-    ?>
-  </form>
+
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#tab-1">Your Custom Post Types</a></li>
+    <li><a href="#tab-2">Add Custom Post Types</a></li>
+    <li><a href="#tab-3">Export</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="tab-1" class="tab-pane active">
+
+     <h3>Manage your Custom Post Types</h3>
+
+     <?php
+      $options = get_option('guisopo_plugin_cpt');
+
+      echo '<table class="cpt-table"><tr><th>ID</th><th>Singular Name</th><th>Plural Name</th><th class="text-center">Public</th><th class="text-center">Archive</th><th class="text-center">Actions</th></tr>';
+
+      foreach($options as $option) {
+        echo "<tr><td>{$option['post_type']}</td><td>{$option['singular_name']}</td><td>{$option['plural_name']}</td><td class=\"text-center\">{$option['public']}</td><td class=\"text-center\">{$option['has_archive']}</td><td class=\"text-center\"><a href=\"#\">EDIT</a> - <a href=\"#\">DELETE</a></td></tr>";
+      }
+
+      echo '</table>';
+     ?>
+
+    </div>
+
+    <div id="tab-2" class="tab-pane">
+
+      <form action="options.php" method="post">
+        <?php
+          settings_fields( 'guisopo_plugin_cpt_settings' );
+          do_settings_sections( 'guisopo_cpt' );
+          submit_button();
+        ?>
+      </form>
+
+    </div>
+
+    <div id="tab-3" class="tab-pane">
+      <h3>Export your Custom Post Types</h3>
+    </div>
+
+  </div>
+
 
 </div>

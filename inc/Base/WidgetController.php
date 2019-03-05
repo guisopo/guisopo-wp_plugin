0289e298df9d1ba7048598a23ffd12168c3377d1
guisopo <solerguillermo@hotmail.com>
2019-03-05 18:50:17 +0100

    Cleaned up widget controller because we do not need its subpage

--- inc/Base/WidgetController.php
+++ inc/Base/WidgetController.php
@@ -4,8 +4,6 @@
  */
 namespace Inc\Base;
 
-use \Inc\Api\SettingsApi;
-use \Inc\Base\BaseController;
 use \Inc\Api\Callbacks\AdminCallbacks;
 
 class WidgetController extends BaseController
@@ -19,25 +17,6 @@ class WidgetController extends BaseController
     // Interrupt if $activated is false
     if( ! $this->activated( 'media_widget' ) ) return;
 
-    $this->settings = new SettingsApi();
-
-    $this->callbacks = new AdminCallbacks();
-
-    $this->setSubpages();
-
-    $this->settings->addSubpages( $this->subpages )->register();
   }
 
-  public function setSubpages() {
-    $this->subpages = array(
-      array(
-        'parent_slug' => 'guisopo_plugin',
-        'page_title' => 'Widgets Manager',
-        'menu_title' => 'Widgets Manager',
-        'capability' => 'manage_options',
-        'menu_slug' => 'guisopo_widget',
-        'callback' => array( $this->callbacks, 'adminWidget' )
-      )
-    );
-  }
 }

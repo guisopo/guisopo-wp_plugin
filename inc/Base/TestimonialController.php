8c8e7703023d1793cbd57b3cc6d0f553413a9722
guisopo <solerguillermo@hotmail.com>
2019-03-07 14:24:05 +0100

    Customized testimonial post columns

--- inc/Base/TestimonialController.php
+++ inc/Base/TestimonialController.php
@@ -22,6 +22,8 @@ class TestimonialController extends BaseController
    add_action( 'init', array( $this, 'testimonial_cpt' ) );
    add_action( 'add_meta_boxes', array(  $this, 'add_meta_boxes' ) );
    add_action( 'save_post', array( $this, 'save_meta_box' ) );
+   add_action( 'manage_testimonial_posts_columns', array($this, 'set_custom_columns' ) );
+   add_action( 'manage_testimonial_posts_custom_column', array($this, 'set_custom_columns_data' ), 10, 2 );
   }
 
   public function testimonial_cpt() {
@@ -146,11 +148,50 @@ class TestimonialController extends BaseController
     $data = array(
       'name' => sanitize_text_field( $_POST['guisopo_testimonial_name'] ), // same as input name
       'email' => sanitize_text_field( $_POST['guisopo_testimonial_email'] ),
-      'approved' => sanitize_text_field( $_POST['guisopo_testimonial_approved'] ),
-      'featured' => sanitize_text_field( $_POST['guisopo_testimonial_featured'] )
+      'approved' => isset( $_POST['guisopo_testimonial_approved'] ) ? 1 : 0,
+      'featured' => isset( $_POST['guisopo_testimonial_featured'] ) ? 1 : 0
     );
     update_post_meta($post_id, '_guisopo_testimonial_key', $data);
 
+  }
+
+  public function set_custom_columns($columns) {
+    // To set the order of our columns, before creating our custom ones we need to unset the default ones
+    $title = $columns['title'];
+    $date = $columns['date'];
+    unset( $columns['title'], $columns['date'] );
+
+    $columns['name'] = 'Author Name';
+    $columns['title'] = $title;
+    $columns['approved'] = 'Approved';
+    $columns['featured'] = 'Featured';
+    $columns['date'] = $date;
+
+    return $columns;
+  }
+
+  public function set_custom_columns_data($column, $post_id) {
+
+    $data = get_post_meta( $post_id, '_guisopo_testimonial_key', true );
+
+    $name = isset($data['name']) ? $data['name'] : '' ;
+    $email = isset($data['email']) ? $data['email'] : '' ;
+    $approved = ( isset($data['approved']) && $data['approved'] === 1 ) ? '<strong>YES</strong>' :  'NO';
+    $featured = ( isset($data['featured']) && $data['featured'] === 1 ) ? '<strong>YES</strong>' :  'NO';
+
+    switch($column) {
+      case 'name':
+        echo '<strong>' . $name . '</strong><br/><a href="mailto:' . $email . '">' . $email . '</a>';
+        break;
+
+      case 'approved':
+        echo $approved;
+        break;
+      
+      case 'featured':
+        echo $featured;
+        break;
+    }
   }
 
 }

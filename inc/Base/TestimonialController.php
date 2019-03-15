0f188c6e9da9b3fc3c06e1fb61e3d454b3f1b3e0
guisopo <solerguillermo@hotmail.com>
2019-03-15 12:00:05 +0100

    Created slider template with its own wp_query

--- inc/Base/TestimonialController.php
+++ inc/Base/TestimonialController.php
@@ -29,8 +29,8 @@ class TestimonialController extends BaseController
     add_filter( 'manage_edit-testimonial_sortable_columns', array($this, 'set_custom_columns_sortable') );
 
     $this->setShortcodePage();
-
     add_shortcode( 'testimonial-form', array( $this, 'testimonial_form' ) );
+    add_shortcode( 'testimonial-slideshow', array( $this, 'testimonial_slideshow' ) );
 
     add_action( 'wp_ajax_submit_testimonial', array( $this, 'submit_testimonial') );
     add_action( 'wp_ajax_nopriv_submit_testimonial', array( $this, 'submit_testimonial') );
@@ -87,6 +87,14 @@ class TestimonialController extends BaseController
     echo "<link rel=\"stylesheet\" href=\"$this->plugin_url/assets/form.css\" type=\"text/css\" media=\"all\" />";
     require_once( "$this->plugin_path/templates/contact-form.php" );
     echo "<script src=\"$this->plugin_url/src/js/form.js\"></script>";
+    return ob_get_clean();
+  }
+
+  public function testimonial_slideshow() {
+    ob_start(); // Video 50
+    echo "<link rel=\"stylesheet\" href=\"$this->plugin_url/assets/slider.css\" type=\"text/css\" media=\"all\" />";
+    require_once( "$this->plugin_path/templates/slider.php" );
+    echo "<script src=\"$this->plugin_url/src/js/slider.js\"></script>";
     return ob_get_clean();
   }
 

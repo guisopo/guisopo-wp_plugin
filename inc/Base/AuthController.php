05e6fe4c8a7bb3e508aa579df7a18ac6c38712b6
guisopo <solerguillermo@hotmail.com>
2019-03-18 09:11:07 +0100

    Clean up authcontroller file

--- inc/Base/AuthController.php
+++ inc/Base/AuthController.php
@@ -4,40 +4,16 @@
  */
 namespace Inc\Base;
 
-use \Inc\Api\SettingsApi;
 use \Inc\Base\BaseController;
-use \Inc\Api\Callbacks\AdminCallbacks;
 
 class AuthController extends BaseController
 {
-  public $callbacks;
-
-  public $subpages = array();
 
   public function register() {
     
    // Interrupt if $activated is false
    if( ! $this->activated( 'login_manager' ) ) return;
 
-    $this->settings = new SettingsApi();
-
-    $this->callbacks = new AdminCallbacks();
-
-    $this->setSubpages();
-
-    $this->settings->addSubpages( $this->subpages )->register();
   }
-
-  public function setSubpages() {
-    $this->subpages = array(
-      array(
-        'parent_slug' => 'guisopo_plugin',
-        'page_title' => 'Login Manager',
-        'menu_title' => 'Login Manager',
-        'capability' => 'manage_options',
-        'menu_slug' => 'guisopo_auth',
-        'callback' => array( $this->callbacks, 'adminAuth' )
-      )
-    );
-  }
+  
 }

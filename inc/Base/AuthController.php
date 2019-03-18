ac3a2eae526c526c9108c56d53103a02e47940d1
guisopo <solerguillermo@hotmail.com>
2019-03-18 10:24:57 +0100

    Added action to authcontroller to enqueue auth scripts

--- inc/Base/AuthController.php
+++ inc/Base/AuthController.php
@@ -12,9 +12,15 @@ class AuthController extends BaseController
   public function register() {
     
    // Interrupt if $activated is false
-   if( ! $this->activated( 'login_manager' ) ) return;
+    if( ! $this->activated( 'login_manager' ) ) return;
 
-   add_action( 'wp_head', array($this, 'add_auth_template') );
+    add_action( 'wp_enqueue_scripts', array($this, 'enqueue') );
+    add_action( 'wp_head', array($this, 'add_auth_template') );
+  }
+
+  public function enqueue() {
+    wp_enqueue_style( 'authStyle', $this->plugin_url . 'assets/auth.scss' );
+    wp_enqueue_script( 'authScript', $this->plugin_url . 'assets/auth.js' );
   }
 
   public function add_auth_template() {
@@ -24,5 +30,6 @@ class AuthController extends BaseController
       load_template( $file, true);
     }
   }
+
 
 }

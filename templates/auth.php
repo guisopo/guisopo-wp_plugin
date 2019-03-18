c513202e6e2445671a86e06ecbba525a4f149903
guisopo <solerguillermo@hotmail.com>
2019-03-18 09:25:21 +0100

    Created ajax login template

---
+++ templates/auth.php
@@ -0,0 +1,5 @@
+<form id="guisopo-auth-form" action="#" method="post" data-url="<?php echo admin_url('admin.ajax.php') ?>">
+  <div class="auth-button">
+    <input type="button"  value="Login" id="guisopo-show-auth-form">
+  </div>
+</form>

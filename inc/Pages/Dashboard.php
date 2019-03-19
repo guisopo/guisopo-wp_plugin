1cdd2b1e5c6fa6a9a5665ddf2b4dc2ad5b1f1203
guisopo <solerguillermo@hotmail.com>
2019-03-19 16:32:36 +0100

    Clean up Dashboard file

--- inc/Pages/Dashboard.php
+++ inc/Pages/Dashboard.php
@@ -36,13 +36,13 @@ class Dashboard extends BaseController
   public function setPages() {
     $this->pages = [ 
       [
-        'page_title' => 'Guisopo Plugin',
-        'menu_title' => 'Guisopo',
-        'capability' => 'manage_options',
-        'menu_slug' => 'guisopo_plugin',
-        'callback' => array( $this->callbacks, 'adminDashboard'),
-        'icon_url' => 'dashicons-store',
-        'position' => 110
+        'page_title'  => 'Guisopo Plugin',
+        'menu_title'  => 'Guisopo',
+        'capability'  => 'manage_options',
+        'menu_slug'   => 'guisopo_plugin',
+        'callback'    => array( $this->callbacks, 'adminDashboard'),
+        'icon_url'    => 'dashicons-store',
+        'position'    => 110
       ]
     ];
   }
@@ -52,9 +52,9 @@ class Dashboard extends BaseController
     $args = array();
 
     $args[] = array(
-      'option_group' => 'guisopo_plugin_settings',
-      'option_name' => 'guisopo_plugin', // Should be identical to page arg in setFields
-      'callback'  => array($this->callbacks_manager, 'checkboxSanitize')
+      'option_group'  => 'guisopo_plugin_settings',
+      'option_name'   => 'guisopo_plugin', // Should be identical to page arg in setFields
+      'callback'      => array($this->callbacks_manager, 'checkboxSanitize')
     );
 
     $this->settings->setSettings( $args );
@@ -63,10 +63,10 @@ class Dashboard extends BaseController
   public function setSections() {
     $args = [
         [
-          'id' => 'guisopo_admin_index',
-          'title' => 'Settings Manager',
+          'id'        => 'guisopo_admin_index',
+          'title'     => 'Settings Manager',
           'callback'  => array($this->callbacks_manager, 'adminSectionManager'),
-          'page'  => 'guisopo_plugin'
+          'page'      => 'guisopo_plugin'
         ]
       ];
 
@@ -76,15 +76,15 @@ class Dashboard extends BaseController
   public function setFields() {
     foreach($this->managers as $key => $value) {
       $args[] = array(
-                  'id' => $key,
-                  'title' => $value,
+                  'id'        => $key,
+                  'title'     => $value,
                   'callback'  => array($this->callbacks_manager, 'checkboxField'),
-                  'page'  => 'guisopo_plugin', // Should be identical to the option_name arg of the setSettings
-                  'section' => 'guisopo_admin_index',  //  Same as id of the section to be printed
+                  'page'      => 'guisopo_plugin', // Should be identical to the option_name arg of the setSettings
+                  'section'   => 'guisopo_admin_index',  //  Same as id of the section to be printed
                   'args' => array(
                     'option_name' => 'guisopo_plugin',
-                    'label_for' =>  $key, // Label should always get the ID in order to get that option for the callback
-                    'class' => 'ui-toggle'
+                    'label_for'   =>  $key, // Label should always get the ID in order to get that option for the callback
+                    'class'       => 'ui-toggle'
                   )
                 );
     }

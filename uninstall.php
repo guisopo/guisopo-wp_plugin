311626fdc78b9c97755dc22e3fbdedb5e06be3ca
guisopo <solerguillermo@hotmail.com>
2019-03-27 10:03:35 +0100

    added comment

--- uninstall.php
+++ uninstall.php
@@ -23,7 +23,7 @@
 
 // Access DB via SQL
 global $wpdb;
-
+// !!! Check if I need to use prepare() in order to escape variables passed to SQL. page 117 WP book
 $wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" );
 $wpdb->query( "DELETE FROM wp_postsmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
 $wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );
